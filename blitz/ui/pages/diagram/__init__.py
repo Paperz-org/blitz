9f3684416cb86a77934aa00d0844f90d70d5f116
Maxime de Pachtere <maxime.depachtere@gmail.com>
2024-02-25 22:13:44 +0100

    continue adding component and remove js as much as possible

--- blitz/ui/pages/diagram/__init__.py
+++ blitz/ui/pages/diagram/__init__.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 from nicegui import ui
 from blitz.ui.pages.base import BasePage
 from blitz.ui.components.buttons.icon import IconButton
 
 
-class MermaidPage(BasePage):
+class Page(BasePage):
     PAGE_NAME = "ERD"
     ZoomButton = IconButton.variant(classes="border rounded-sm")
 
@@ -11,16 +12,13 @@ class MermaidPage(BasePage):
         self._width = 100
 
     def remove_style(self) -> None:
-        ui.run_javascript(
-            """
-            var svg = document.querySelector('svg');
-            svg.removeAttribute("style");
-            """
-        )
+        with open(Path(__file__).parent / "./remove_style.js") as f:
+            ui.run_javascript(f.read())
 
     def zoom_svg(self) -> None:
         self._width += 50
         self.remove_style()
+        # Can't put it in a file without a better integration like a bridge to js function or something like this
         ui.run_javascript(
             f"""
             var svg = document.querySelector('svg');
@@ -33,6 +31,7 @@ class MermaidPage(BasePage):
         if self._width < 100:
             self._width = 100
         self.remove_style()
+        # Can't put it in a file without a better integration like a bridge to js function or something like this
         ui.run_javascript(
             f"""
             var svg = document.querySelector('svg');
@@ -49,3 +48,6 @@ class MermaidPage(BasePage):
         with ui.footer().classes("w-full justify-start "):
             self.ZoomButton(icon="zoom_in", on_click=self.zoom_svg)
             self.ZoomButton(icon="zoom_out", on_click=self.unzoom_svg)
+
+
+MermaidPage = Page
